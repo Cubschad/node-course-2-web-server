80d26b1f73bbcc5b855e281bec702e150fc371fd
Chad Oliver <chadoliver87@gmail.com>
2018-05-20 15:51:13 -0600

    Setup Projects page and update Header

--- server.js
+++ server.js
@@ -10,19 +10,20 @@ app.set('view engine', 'hbs');
 
 app.use((req, res, next) => {
     var now = new Date().toString();
-    var log = `${now}: ${req.method} ${req.url}`
+    var log = `${now}: ${req.method} ${req.url}`;
+
     console.log(log);
     fs.appendFile('server.log', log + '\n', (err) => {
         if (err) {
             console.log('Unable to append to server.log');
         }
-    })
+    });
     next()
 });
 
-app.use((req, res, next) => {
-    res.render('maintenance.hbs');
-});
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
 
 app.use(express.static(__dirname + '/public'));
 
@@ -53,6 +54,13 @@ app.get('/bad', (req, res) => {
         errorMessage: 'Unable to handle request'
     });
 });
+
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects Page',
+        welcomeMessage: 'Portfolio page here'
+    })
+})
 
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`);
